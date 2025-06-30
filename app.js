85ea14b1e059a29f7873bcb1e44d70fb2f26b857
Ashish Kumar <helloashish20@gmail.com>
2025-06-30 23:13:32 +0530

    a

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
@@ -6,6 +7,7 @@ const methodOverride =  require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
 const session = require("express-session");
+const MongoStore = require('connect-mongo');
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
@@ -15,7 +17,8 @@ const listingRouter = require("./routes/listing.js");
 const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+//const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const dbUrl = process.env.MONGO_URL;
 
 
 main()
@@ -25,7 +28,7 @@ main()
 .catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect(MONGO_URL);
+  await mongoose.connect(dbUrl);
   
 }
 app.set("view engine","ejs");
@@ -35,7 +38,20 @@ app.use(methodOverride("_method"));
 app.engine('ejs',ejsMate);
 app.use(express.static(path.join(__dirname,"/public")));
 
+const store = MongoStore.create({
+  mongoUrl:dbUrl,
+  crypto:{
+    secret:"mysupersecretcode",
+  },
+  touchAfter: 24 * 3600,
+});
+
+store.on("error", ()=>{
+  console.log("ERROR in MONGO SESSION STORE", err);
+});
+
 const sessionOptions = {
+  store,
   secret: "mysupersecretcode",
   resave: false,
   saveUninitialized: true,
@@ -46,9 +62,11 @@ const sessionOptions = {
   },
 };
 
-app.get("/",(req,res)=>{
-  res.send("Hi, I am root");
-});
+// app.get("/",(req,res)=>{
+//   res.send("Hi, I am root");
+// });
+
+
 
 app.use(session(sessionOptions));
 app.use(flash());
--- routes/listing.js
+++ routes/listing.js
@@ -4,20 +4,22 @@ const wrapAsync = require("../utils/wrapAsync.js");
 const Listing = require("../models/listing.js");
 const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
 const listingController = require("../controllers/listings.js");
-
-// Define routes
-
-//New Route
-router.get("/new", isLoggedIn, listingController.renderNewForm);
+const multer = require("multer");
+const { storage } = require("../cloudConfig.js");
+const upload = multer({ storage });
 
 router
   .route("/")
   .get(wrapAsync(listingController.index))
   .post(
     isLoggedIn,
+    upload.single("listing[image]"),
     validateListing,
     wrapAsync(listingController.createListing)
   );
+
+//New Route
+router.get("/new", isLoggedIn, listingController.renderNewForm);
 
 router
   .route("/:id")
@@ -25,6 +27,7 @@ router
   .put(
     isLoggedIn,
     isOwner,
+    upload.single("listing[image]"),
     validateListing,
     wrapAsync(listingController.updateListing)
   )
